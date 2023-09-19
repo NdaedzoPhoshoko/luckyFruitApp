ea2f3e994f2da3b0b98cc448380625e9ac6ccbf9
Ndaedzo Phoshoko <ndaedzo.clement.phoshoko@gamil.com>
2023-09-19 22:08:45 +0200

    2nd Commit v.2

--- app/src/main/java/com/example/appwithnegivablemenu/GiveAppleActivity.java
+++ app/src/main/java/com/example/appwithnegivablemenu/GiveAppleActivity.java
@@ -1,8 +1,12 @@
 package com.example.appwithnegivablemenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -26,9 +30,10 @@ public class GiveAppleActivity extends AppCompatActivity {
         title.setText(username+" your lucky fruit is...");
 
         //array storing five fruit names
-        String[] my_fruits = {"Apple", "Banana", "Cherry", "Lemon", "Papaya"};
+        String[] my_fruits = {"Apple", "Banana", "Cherry", "Lemon", "Papaya", "Mango", "Pears", "Orange", "Grape", "Avocado"};
         //setting fruitName to randomFruit
-        fruitName.setText(luckyFruit(my_fruits));
+        String luckyFruitName = luckyFruit(my_fruits);
+        fruitName.setText(luckyFruitName);
     }
 
     /*
@@ -41,4 +46,46 @@ public class GiveAppleActivity extends AppCompatActivity {
         return fruits[index.nextInt(fruits.length-1)]; //bounds for index are 0 to size-1
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.base_menu, menu);
+        return true;
+    }
+
+    /*
+    This method is overridden to allow functionality for items clicked on the menu
+     */
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if(itemId == R.id.home_item)
+        {
+            intentTo(MainActivity.class); //intent to MainActivity.class
+        } else if (itemId == R.id.share_item) {
+            //receiving username from the intent of MainActivity.java
+            String username = getIntent().getStringExtra("username");
+            //initialize fruitName
+            fruitName = (TextView) findViewById(R.id.displayFruitTextView);
+            intentShareTo(username, fruitName.getText().toString()); //intent to another activity with extra values
+        }else{ //for id for last item: quiting the app
+            //todo add new code for quiting the app here
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //method 1: intent to another Activity without passing values
+    public void intentTo(Class toActivity)//method to intent to any other activity
+    {
+        Intent i = new Intent(GiveAppleActivity.this, toActivity);
+        startActivity(i);
+    }
+    //method 2: intent to share values to other activities
+    public void intentShareTo(String username, String fruitName)//method to intent to any other activity
+    {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("text/plain");
+        i.putExtra(Intent.EXTRA_TEXT, "Hey, "+ username + " got "+ fruitName + " as lucky fruit today");
+        i.putExtra(Intent.EXTRA_SUBJECT, "My Lucky Fruit Today");
+        startActivity(Intent.createChooser(i, "Sharing fruit with"));
+    }
 }
--- app/src/main/java/com/example/appwithnegivablemenu/MainActivity.java
+++ app/src/main/java/com/example/appwithnegivablemenu/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -52,5 +53,4 @@ public class MainActivity extends AppCompatActivity {
         i.putExtra("username", userName);
         startActivity(i);
     }
-
 }
